8ebee78bdb2c7910513091ea041ce8d20c09f023
MARIAJOSE2005 <mintriago6546@utm.edu.ec>
2026-05-28 18:23:10 -0500

    Refactor validators.js to add text validation function

--- aplicaciones_moviles_utm-master/js/utils/validators.js
+++ aplicaciones_moviles_utm-master/js/utils/validators.js
@@ -12,3 +12,12 @@ export function capacidadValida(capacidad){
 
     return capacidad >= 10;
 }
+// Limpia y valida texto
+export function validarTexto(texto){
+
+    if(!texto){
+        return "";
+    }
+
+    return texto.trim();
+}
