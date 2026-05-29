ec93ac9fda79561cc4e8c64d30489c4a0cc854f8
lore2341 <140657037+lore2341@users.noreply.github.com>
2026-05-28 22:22:00 -0500

    Update storage.js

--- aplicaciones_moviles_utm-master/js/models/service/storage.js
+++ aplicaciones_moviles_utm-master/js/models/service/storage.js
@@ -1,36 +1,73 @@
-const STORAGE_KEY = "rutasUniversitarias"; 
-// Guardar rutas en localStorage 
-export function guardarRutasStorage(rutas) { 
-    try { 
-        localStorage.setItem( 
-            STORAGE_KEY, 
-            JSON.stringify(rutas) 
-        ); 
-        return true; 
-    } catch (error) { 
-        console.error( "Error al guardar rutas:", error ); 
-        return false; 
-    } 
-} 
-// Cargar rutas desde localStorage 
-export function cargarRutasStorage() { 
-    try { 
-        const rutasGuardadas = localStorage.getItem(STORAGE_KEY);
-         if (!rutasGuardadas) { 
-            return []; 
-        } 
-        const datos = JSON.parse(rutasGuardadas); 
-        // Validar estructura 
-        if (!Array.isArray(datos)) { 
-            return []; 
-        } 
-        return datos; 
-    } catch (error) { 
-        console.error( "Error al cargar rutas:", error ); 
-        return []; 
-    } 
-} 
-// Eliminar datos almacenados 
-export function limpiarStorage() { 
-    localStorage.removeItem( STORAGE_KEY ); 
+```javascript id="8k4mwu"
+const STORAGE_KEY = "rutasUniversitarias";
+
+
+// GUARDAR RUTAS
+export function guardarRutasStorage(rutas) {
+
+    try {
+
+        localStorage.setItem(
+            STORAGE_KEY,
+            JSON.stringify(rutas)
+        );
+
+        return true;
+
+    } catch (error) {
+
+        console.error(
+            "Error al guardar rutas:",
+            error
+        );
+
+        return false;
+    }
 }
+
+
+// CARGAR RUTAS
+export function cargarRutasStorage() {
+
+    try {
+
+        const rutasGuardadas =
+            localStorage.getItem(STORAGE_KEY);
+
+        // SI NO EXISTEN DATOS
+        if (!rutasGuardadas) {
+
+            return [];
+        }
+
+        const datos =
+            JSON.parse(rutasGuardadas);
+
+        // VALIDAR ESTRUCTURA
+        if (!Array.isArray(datos)) {
+
+            return [];
+        }
+
+        return datos;
+
+    } catch (error) {
+
+        console.error(
+            "Error al cargar rutas:",
+            error
+        );
+
+        return [];
+    }
+}
+
+
+// LIMPIAR STORAGE
+export function limpiarStorage() {
+
+    localStorage.removeItem(
+        STORAGE_KEY
+    );
+}
+```
